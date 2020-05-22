8afe7384470e679d31ea94bfb6ccc8bcc04f7b5f
Sean Hoar <sean.hoar@gmail.com>
2020-05-22 07:59:44 -0400

    create initial homeReducer from action cases

--- src/reducers/home_reducer.js
+++ src/reducers/home_reducer.js
@@ -1,8 +1,61 @@
-export const initialState = state = {
-    movies: [],
-    heroImage: null,
-    loading: false,
-    currentPage: 0,
-    totalPages: 0,
-    searchTerm: ''
+import {
+  CLEAR_MOVIES,
+  GET_POPULAR_MOVIES,
+  LOAD_MORE_MOVIES,
+  SEARCH_MOVIES,
+  SHOW_LOADING_SPINER,
+} from "../actions";
+
+export const initialState = (state = {
+  movies: [],
+  heroImage: null,
+  loading: false,
+  currentPage: 0,
+  totalPages: 0,
+  searchTerm: "",
+});
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case GET_POPULAR_MOVIES:
+      return {
+        ...state,
+        movies: action.payload.results,
+        heroImage: state.heroImage || action.payload.results[0],
+        loading: false,
+        currentPage: action.payload.page,
+        totalPages: action.payload.total_pages,
+        searchTerm: "",
+      };
+
+    case LOAD_MORE_MOVIES:
+      return {
+        ...state,
+        movies: [...state.movies, ...state.action.payload.results],
+        loading: false,
+        currentPage: action.payload.page,
+        totalPage: action.payload.total_pages,
+      };
+
+    case SEARCH_MOVIES:
+      return {
+        ...state,
+        movies: action.payload.results,
+        loading: false,
+        currentPage: action.payload.page,
+        totalPages: action.payload.total_pages,
+        searchTerm: action.payload.searchTerm,
+      };
+
+    case CLEAR_MOVIES:
+      return {
+        ...state,
+        movies: [],
+      };
+
+    case SHOW_LOADING_SPINER:
+      return { ...state, loading: true };
+    default:
+      return state;
   }
+}
